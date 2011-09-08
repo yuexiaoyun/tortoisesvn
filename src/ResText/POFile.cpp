yuexiaoyun/tortoisesvn

commit 1eac2a8cc20edbf4a85991c40e6aba4e38c4df15

Reduce stack size by reserving memory on the heap instead.
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21936 154be670-7969-47af-9ab0-c6fb378d2295

src/ResText/POFile.cpp
@@ -62,7 +62,7 @@ BOOL CPOFile::ParseFile(LPCTSTR szPath, BOOL bUpdateExisting, bool bAdjustEOLs)
         _ftprintf(stderr, _T("can't open input file %s\n"), szPath);
         return FALSE;
     }
-    TCHAR line[2*MAX_STRING_LENGTH];
+    TCHAR * line = new TCHAR[2*MAX_STRING_LENGTH];
     std::vector<std::wstring> entry;
     do
     {
@@ -146,6 +146,7 @@ BOOL CPOFile::ParseFile(LPCTSTR szPath, BOOL bUpdateExisting, bool bAdjustEOLs)
             entry.push_back(line);
         }
     } while (File.gcount() > 0);
+    delete [] line;
     printf(File.getloc().name().c_str());
     File.close();
     RESOURCEENTRY emptyentry;

src/TortoiseIDiff/PicWindow.cpp
@@ -1232,8 +1232,8 @@ void CPicWindow::Paint(HWND hwnd)
             SetBkColor(memDC, transparentColor);
             if (bShowInfo)
             {
-                TCHAR infostring[8192];
-                BuildInfoString(infostring, _countof(infostring), false);
+                TCHAR * infostring = new TCHAR[8192];
+                BuildInfoString(infostring, 8192, false);
                 // set the font
                 NONCLIENTMETRICS metrics = {0};
                 metrics.cbSize = sizeof(NONCLIENTMETRICS);
@@ -1268,6 +1268,7 @@ void CPicWindow::Paint(HWND hwnd)
                 DrawText(memDC, infostring, -1, &m_inforect, DT_EDITCONTROL | DT_EXPANDTABS | DT_LEFT | DT_VCENTER);
                 SelectObject(memDC, (HGDIOBJ)hFontOld);
                 DeleteObject(hFont);
+                delete [] infostring;
             }
         }
         else

src/TortoiseIDiff/TortoiseIDiff.cpp
@@ -17,7 +17,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 #include "stdafx.h"
-#include "MainWindow.h"
+#include "mainWindow.h"
 #include "CmdLineParser.h"
 #include "registry.h"
 #include "LangDll.h"
@@ -76,34 +76,34 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     curHand = (HCURSOR)LoadImage(hInst, MAKEINTRESOURCE(IDC_PANCUR), IMAGE_CURSOR, 0, 0, LR_DEFAULTSIZE);
     curHandDown = (HCURSOR)LoadImage(hInst, MAKEINTRESOURCE(IDC_PANDOWNCUR), IMAGE_CURSOR, 0, 0, LR_DEFAULTSIZE);
 
-    CMainWindow mainWindow(hResource);
-    mainWindow.SetRegistryPath(_T("Software\\TortoiseSVN\\TortoiseIDiffWindowPos"));
+    CMainWindow * mainWindow = new CMainWindow(hResource);
+    mainWindow->SetRegistryPath(_T("Software\\TortoiseSVN\\TortoiseIDiffWindowPos"));
 
-    mainWindow.SetLeft(parser.HasVal(_T("left")) ? parser.GetVal(_T("left")) : _T(""), parser.HasVal(_T("lefttitle")) ? parser.GetVal(_T("lefttitle")) : _T(""));
-    mainWindow.SetRight(parser.HasVal(_T("right")) ? parser.GetVal(_T("right")) : _T(""), parser.HasVal(_T("righttitle")) ? parser.GetVal(_T("righttitle")) : _T(""));
-    if (mainWindow.RegisterAndCreateWindow())
+    mainWindow->SetLeft(parser.HasVal(_T("left")) ? parser.GetVal(_T("left")) : _T(""), parser.HasVal(_T("lefttitle")) ? parser.GetVal(_T("lefttitle")) : _T(""));
+    mainWindow->SetRight(parser.HasVal(_T("right")) ? parser.GetVal(_T("right")) : _T(""), parser.HasVal(_T("righttitle")) ? parser.GetVal(_T("righttitle")) : _T(""));
+    if (mainWindow->RegisterAndCreateWindow())
     {
         hAccelTable = LoadAccelerators(hResource, MAKEINTRESOURCE(IDR_TORTOISEIDIFF));
         if (!parser.HasVal(_T("left")))
         {
-            PostMessage(mainWindow, WM_COMMAND, ID_FILE_OPEN, 0);
+            PostMessage(*mainWindow, WM_COMMAND, ID_FILE_OPEN, 0);
         }
         if (parser.HasKey(_T("overlay")))
         {
-            PostMessage(mainWindow, WM_COMMAND, ID_VIEW_OVERLAPIMAGES, 0);
+            PostMessage(*mainWindow, WM_COMMAND, ID_VIEW_OVERLAPIMAGES, 0);
         }
         if (parser.HasKey(_T("fit")))
         {
-            PostMessage(mainWindow, WM_COMMAND, ID_VIEW_FITTOGETHER, 0);
+            PostMessage(*mainWindow, WM_COMMAND, ID_VIEW_FITTOGETHER, 0);
         }
         if (parser.HasKey(_T("showinfo")))
         {
-            PostMessage(mainWindow, WM_COMMAND, ID_VIEW_IMAGEINFO, 0);
+            PostMessage(*mainWindow, WM_COMMAND, ID_VIEW_IMAGEINFO, 0);
         }
         // Main message loop:
         while (GetMessage(&msg, NULL, 0, 0))
         {
-            if (!TranslateAccelerator(mainWindow, hAccelTable, &msg))
+            if (!TranslateAccelerator(*mainWindow, hAccelTable, &msg))
             {
                 TranslateMessage(&msg);
                 DispatchMessage(&msg);
@@ -111,6 +111,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
         }
         return (int) msg.wParam;
     }
+    delete mainWindow;
     langDLL.Close();
     DestroyCursor(curHand);
     DestroyCursor(curHandDown);

src/TortoiseProc/Commands/RevisionGraphCommand.cpp
@@ -26,17 +26,18 @@ bool RevisionGraphCommand::Execute()
     CString val = parser.GetVal(_T("pegrev"));
     SVNRev pegrev = val.IsEmpty() ? SVNRev() : SVNRev(val);
 
-    CRevisionGraphDlg dlg;
-    dlg.SetPath(cmdLinePath.GetUIPathString());
-    dlg.SetPegRevision(pegrev);
+    CRevisionGraphDlg * dlg = new CRevisionGraphDlg();
+    dlg->SetPath(cmdLinePath.GetUIPathString());
+    dlg->SetPegRevision(pegrev);
     if (parser.HasVal(L"output"))
     {
-        dlg.SetOutputFile(parser.GetVal(L"output"));
+        dlg->SetOutputFile(parser.GetVal(L"output"));
         if (parser.HasVal(L"options"))
-            dlg.SetOptions(parser.GetLongVal(L"options"));
-        dlg.StartHidden();
+            dlg->SetOptions(parser.GetLongVal(L"options"));
+        dlg->StartHidden();
     }
-    dlg.DoModal();
+    dlg->DoModal();
+    delete dlg;
 
     return true;
 }

src/Utils/MiscUI/FileDropEdit.h
@@ -20,6 +20,7 @@
 
 #include "DragDropImpl.h"
 #include "UnicodeUtils.h"
+#include "auto_buffer.h"
 
 /**
  * \ingroup Utils
@@ -36,7 +37,7 @@ public:
             if(medium.pstm != NULL)
             {
                 const int BUF_SIZE = 10000;
-                char buff[BUF_SIZE+1];
+                auto_buffer<char> buff(BUF_SIZE+1);
                 ULONG cbRead=0;
                 HRESULT hr = medium.pstm->Read(buff, BUF_SIZE, &cbRead);
                 if (SUCCEEDED(hr) && (cbRead > 0) && (cbRead < BUF_SIZE))
@@ -67,7 +68,7 @@ public:
             if(medium.pstm != NULL)
             {
                 const int BUF_SIZE = 10000;
-                TCHAR buff[BUF_SIZE+1];
+                auto_buffer<WCHAR> buff(BUF_SIZE+1);
                 ULONG cbRead=0;
                 HRESULT hr = medium.pstm->Read(buff, BUF_SIZE, &cbRead);
                 if (SUCCEEDED(hr) && (cbRead > 0) && (cbRead < BUF_SIZE))

commit 22dad24739237de1713c3df65701fcb34965c2cc

Fix bug introduced in r21936: sizeof() doesn't work here anymore.

src/ResText/POFile.cpp
@@ -66,7 +66,7 @@ BOOL CPOFile::ParseFile(LPCTSTR szPath, BOOL bUpdateExisting, bool bAdjustEOLs)
     std::vector<std::wstring> entry;
     do
     {
-        File.getline(line, sizeof(line)/sizeof(TCHAR));
+        File.getline(line, 2*MAX_STRING_LENGTH);
         if (line[0]==0)
         {
             //empty line means end of entry!